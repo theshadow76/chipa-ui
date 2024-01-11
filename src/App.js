6b2854617502617fb3300e660e937fbbc64605cd
Vigo Walker <vigopaul05@gmail.com>
2024-01-11 18:22:47 -0300

    sfczfzsdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import HomeImage from './assets/home.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="SidePanel">
+        <div className="child-div-SidePanel">
+          <img className="child-div-SidePanel-img" src={HomeImage}></img>
+          <h3 className="child-div-SidePanel-h3">Home</h3>
+        </div>
+      </div>
     </div>
   );
 }
